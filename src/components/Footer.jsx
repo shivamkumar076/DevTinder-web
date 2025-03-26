e5df3856e0b62e72b2d508f70e5b3be0d82a7959
Shivam Kumar <shivamkumar776499@gmail.com>
2025-03-26 11:47:54 -0700

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+    <footer className="footer sm:footer-horizontal footer-center bg-base-300 fixed bottom-0 text-base-content p-4">
     <aside>
       <p>Copyright © {new Date().getFullYear()} - All right reserved by shivam kumar</p>
     </aside>
